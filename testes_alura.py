c597299b8d56bcae681dceecf5d59071a53350e8
Carla Edila Silveira <rosa.carla@pucpr.edu.br>
2025-02-20 19:40:46 -0300

    Create testes_alura.py

---
+++ testes_alura.py
@@ -0,0 +1,19 @@
+# Impressão configurada com separador " "
+subst = "Python" 
+verbo = "é" 
+adjetivo = "fantástico" print(subst, verbo, adjetivo, sep="_", end="!\n") 
+
+# Soma impossível de inteiro e string 
+idade1 = 10
+idade2 = "20" 
+print(idade1 + idade2)
+
+# Soma possível após conversão de string para inteiro
+idade1 = 10 
+idade2 = int("20") 
+print(idade1 + idade2)
+
+# Concatenação de strings
+nome = "Nico" 
+sobrenome = "Steppat" 
+print(nome + sobrenome) 
