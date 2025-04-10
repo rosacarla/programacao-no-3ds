b3f939346a57c850d9aa0b31a389db57d88bd1b0
Carla Edila Silveira <rosa.carla@pucpr.edu.br>
2025-04-09 22:10:54 -0300

    Create igualdade.py

---
+++ igualdade.py
@@ -0,0 +1,9 @@
+# Solicita ao usuário que insira dois números
+numero1 == float(input("Digite o 1o. número: "))
+numero2 == float(input("Digite o 2o. número: "))
+
+# Verifica comparando se números são iguais
+if numero1 == numero2:
+    print("Os números são iguais.")
+else:
+    print("Os números são diferentes.")
